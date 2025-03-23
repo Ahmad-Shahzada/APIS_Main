67383888d0ed083005c081fb568b5df9429c8977
ahmad <ahmadshahzadrajpoot005@gmail.com>
2025-03-23 22:38:59 +0500

    changes completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import RandomUser from "./components/RandomUser";
 import UserDetail from "./components/UserDetail";
 import Random from "./components/Random";
 
-function App() {
+const AppContent = () => {
+  const location = useLocation();
+  const isUserDetailPage = location.pathname.startsWith("/user/");
+
   return (
-    <Router>
-      <Random/>
+    <>
+      {!isUserDetailPage && <Random />}
       <Routes>
         <Route path="/" element={<RandomUser />} />
         <Route path="/user/:id" element={<UserDetail />} />
       </Routes>
+    </>
+  );
+};
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
     </Router>
   );
 }
